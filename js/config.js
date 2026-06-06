670afc365a1ecfaf790af85c984f2eba3efcfeec
AzxKun <zenx4ukz@gmail.com>
2026-06-06 19:35:07 +0630

    Updated brand name and fixed search

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // config.js – Replace with your own Google Sheet ID and Telegram link
 const CONFIG = {
     SHEET_ID: '18APCzYnE1pa8I26YrGD6_Iqz48QJRJwZE0HbVu08VHo',
-    SHEET_NAME: 'Products',
+    SHEET_NAME: 'TEST18',
     TELEGRAM_USERNAME: 'Hayythithaw',   // change to your Telegram username
     TELEGRAM_LINK: 'https://t.me/LMT_4_HTT'
 };
